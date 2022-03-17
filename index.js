5b54b703b6029970c0e6990d6d86a8ea40dc5478
maliovaniy <alexandr.maliovaniy@whykingz.com>
2022-03-18 01:46:54 +0200

    copy & install logic

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+const commandLineArgs = require('command-line-args')
+const { exec } = require("child_process");
+const fse = require('fs-extra');
+const path = require('path');
+
+const optionDefinitions = [
+    { name: 'src', type: String, multiple: false, defaultOption: true },
+    { name: 'typescript', type: Boolean },
+    { name: 'storage', alias: 's', type: String }
+]
+
+const {src, typescript, storage} = commandLineArgs(optionDefinitions)
+
+const typePath = typescript ? 'typescript' : 'javascript';
+const storagePath = storage ? storage : 'default';
+
+const loadPath = path.join(__dirname, typePath, storagePath);
+const savePath = path.join(process.env.PWD, src);
+
+fse.copySync(loadPath, savePath);
+
+exec(`cd ${savePath} && npm install`, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+console.log("Done!");
